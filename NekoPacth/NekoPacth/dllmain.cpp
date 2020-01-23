01fae88869ca645a23a6fae338ee0d19347c69ed
Soratane <47900658+MZSHproject@users.noreply.github.com>
2020-01-23 17:51:42 +0800

    Update

--- NekoPacth/NekoPacth/dllmain.cpp
+++ NekoPacth/NekoPacth/dllmain.cpp
@@ -1,98 +1,207 @@
 #include "stdafx.h"
 #include "detours.h"
-#include <iostream>
-#include <mutex>
 #pragma comment(lib, "detours.lib")
 using namespace std;
+DWORD BaseAddr = (DWORD)GetModuleHandle(NULL);
 
-//hook´°¿ÚÃ»ÓĞĞ§¹û£¬ÒªÈ¥yscfg.ybnÀïÃæĞŞ¸Ä£¬×¢Òâ±£³ÖÍ·²¿µÄÒ»ÖÂ¡£hook msgbox»áµ¼ÖÂÎŞ·¨ÏÔÊ¾¡£
-//±ß½çÊÇÊı×é£¬ÕÒµ½Ë«×Ö½Ú¼ì²é±í£¬°Ñ81µ½FEµÄÎ»ÖÃÈ«²¿Ìî01¼´¿É¡£
-typedef HFONT(WINAPI* Func_CreateFontIndirectA)(LOGFONTA *lplf);
-PVOID p_CreateFontIndirectA = CreateFontIndirectA;
-PVOID g_pOldCreateWindowExA = CreateWindowExA;
-typedef HWND(WINAPI *pfuncCreateWindowExA)(
-	DWORD dwExStyle,
-	LPCTSTR lpClassName,
-	LPCTSTR lpWindowName,
-	DWORD dwStyle,
-	int x,
-	int y,
-	int nWidth,
-	int nHeight,
-	HWND hWndParent,
-	HMENU hMenu,
-	HINSTANCE hInstance,
-	LPVOID lpParam);
-typedef int (WINAPI *fnMessageboxA)(
+//You should find CharCheckTable in exe.You can find it in TextOutA.
+/*
+00595121  01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01  
+00595131  01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 00  
+00595141  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  
+00595151  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  
+00595161  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  
+00595171  00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01  
+00595181  01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01  
+00595191  01 01 01 01 01 01 01 01 01 01 01 01 00 00 00     
+
+
+007E3994  79 73 62 69 6E 5C 79 73 74 25 30 35 64 2E 79 62  ysbin\yst%05d.yb
+007E39A4  6E                                               n
+
+00B3F0A8   03518665  \Text = "åŠå‚ åªå‚–å‚«å¡ä¸¡ä¸‚ä¹£åƒ„å„˜å„å™å²å‘åŸå¨å”å°å¨å‚‘å‚œå´å„å£ŒåºæŒ§æ„´ä¹£"
+
+
+
+
+void memcopy(void* dest, void* src, size_t size)
+{
+	DWORD oldProtect;
+	VirtualProtect(dest, size, PAGE_EXECUTE_READWRITE, &oldProtect);
+	memcpy(dest, src, size);
+}
+
+void MyCharSet()
+{
+	BYTE Patch1[] = { 0x01 };
+	BYTE Patch2[] = { 0x4D };
+	BYTE Patch3[] = { 0x79 };
+	BYTE Patch4[] = { 0x53 };
+	BYTE Patch5[] = { 0x69 };
+	BYTE Patch6[] = { 0x75 };
+
+	//BYTE Patch7[] = { 0x86 };
+
+	memcopy((void*)(BaseAddr + 0x195140), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195141), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195142), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195143), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195144), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195145), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195146), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195147), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195148), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195149), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19514A), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19514B), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19514C), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19514D), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19514E), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19514F), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195150), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195151), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195152), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195153), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195154), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195155), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195156), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195157), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195158), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195159), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19515A), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19515B), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19515C), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19515D), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19515E), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19515F), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195160), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195161), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195162), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195163), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195164), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195165), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195166), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195167), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195168), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195169), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19516A), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19516B), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19516C), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19516D), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19516E), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19516F), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195170), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195171), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195172), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195173), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195174), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195175), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195176), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195177), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195178), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x195179), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19517A), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19517B), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19517C), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19517D), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19517E), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19517F), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19519D), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19519E), Patch1, sizeof(Patch1));
+	memcopy((void*)(BaseAddr + 0x19519F), Patch1, sizeof(Patch1));
+
+	memcopy((void*)(BaseAddr + 0X3E3994), Patch2, sizeof(Patch2));
+	memcopy((void*)(BaseAddr + 0X3E3995), Patch3, sizeof(Patch3));
+	memcopy((void*)(BaseAddr + 0X3E3996), Patch4, sizeof(Patch4));
+	memcopy((void*)(BaseAddr + 0X3E3997), Patch5, sizeof(Patch5));
+	memcopy((void*)(BaseAddr + 0X3E3998), Patch6, sizeof(Patch6));
+
+}*/
+
+LPWSTR ctowJIS(char* str)
+{
+	DWORD dwMinSize;
+	dwMinSize = MultiByteToWideChar(932, 0, str, -1, NULL, 0); //è®¡ç®—é•¿åº¦
+	LPWSTR out = new wchar_t[dwMinSize];
+	MultiByteToWideChar(932, 0, str, -1, out, dwMinSize);//è½¬æ¢
+	return out;
+}
+
+char* wtocGBK(LPCTSTR str)
+{
+	DWORD dwMinSize;
+	dwMinSize = WideCharToMultiByte(936, NULL, str, -1, NULL, 0, NULL, FALSE); //è®¡ç®—é•¿åº¦
+	char* out = new char[dwMinSize];
+	WideCharToMultiByte(936, NULL, str, -1, out, dwMinSize, NULL, FALSE);//è½¬æ¢
+	return out;
+}
+
+PVOID g_pOldCreateFontIndirectA = CreateFontIndirectA;
+typedef HFONT(WINAPI* PfuncCreateFontIndirectA)(LOGFONTA* lplf);
+HFONT WINAPI NewCreateFontIndirectA(LOGFONTA* lplf)
+{
+	lplf->lfCharSet = GB2312_CHARSET;
+	strcpy(lplf->lfFaceName, "é»‘ä½“");
+
+	return ((PfuncCreateFontIndirectA)g_pOldCreateFontIndirectA)(lplf);
+}
+
+wchar_t szTitle[] = L"ç¡®è®¤";
+typedef int (WINAPI* fnMessageboxA)(
 	_In_opt_ HWND    hWnd,
 	_In_opt_ LPCSTR lpText,
 	_In_opt_ LPCSTR lpCaption,
 	_In_     UINT    uType
 	);
 fnMessageboxA MessageBoxAOLD;
-
-HFONT WINAPI NewCreateFontIndirectA(LOGFONTA *lplf)
-{
-	LOGFONTA lf;
-	memcpy(&lf, lplf, sizeof(LOGFONTA));
-	strcpy(lf.lfFaceName, "ºÚÌå");
-	lf.lfCharSet = GB2312_CHARSET;
-	return (Func_CreateFontIndirectA(p_CreateFontIndirectA))(&lf);
-}
-
-
 int WINAPI MessageBoxAEx(_In_opt_ HWND    hWnd,
 	_In_opt_ LPCSTR lpText,
 	_In_opt_ LPCSTR lpCaption,
 	_In_     UINT    uType)
 {
-	if (lpText == ("¥²©`¥à¤ò½KÁË¤·¤Ş¤¹¡£¤è¤í¤·¤Ç¤¹¤«£¿"))
+	char* Pstr = wtocGBK(ctowJIS((char*)lpText));
+	if (strcmp(Pstr, "ã‚²ãƒ¼ãƒ ã‚’çµ‚äº†ã—ã¾ã™ã€‚ã‚ˆã‚ã—ã„ã§ã™ã‹ï¼Ÿ") == 0)
 	{
-		return MessageBoxAOLD(hWnd, "È·¶¨Òª½áÊøÂğ?", lpCaption, uType);
+		strcpy(Pstr, "å³å°†ç»“æŸæ¸¸æˆã€‚ç¡®è®¤å—ï¼Ÿ");
+
 	}
-	if (lpText == ("¤³¤Á¤é¤Ç¤è¤í¤·¤¤¤Ç¤¹¤«£¿£¨Í¾ÖĞ¤Ç±ä¸ü¤Ï³öÀ´¤Ş¤»¤ó£©"))
+	/*
+	if (strcmp(Pstr, "ã“ã¡ã‚‰ã§ã‚ˆã‚ã—ã„ã§ã™ã‹ï¼Ÿï¼ˆé€”ä¸­ã§å¤‰æ›´ã¯å‡ºæ¥ã¾ã›ã‚“ï¼‰") == 0)
 	{
-		return MessageBoxAOLD(hWnd, "ÕâÑù¿ÉÒÔÂğ£¿£¨ÓÎÏ·Í¾ÖĞ²»ÄÜ¸Ä±äÃû×Ö£©", lpCaption, uType);
+		strcpy(Pstr, "è¿™æ ·å¯ä»¥å—ï¼Ÿï¼ˆä¸­é€”æ— æ³•å†æ”¹å˜ï¼‰");
+
 	}
+	*/
+	return MessageBoxAOLD(hWnd, Pstr, wtocGBK(szTitle), uType);
 }
 
-
-void BeginDetour()
+void Init()
 {
-	DetourTransactionBegin();
-	DetourUpdateThread(GetCurrentThread());
-	DetourAttach(&p_CreateFontIndirectA, NewCreateFontIndirectA);;
+	DWORD BaseAddr = (DWORD)GetModuleHandle(NULL);
 	MessageBoxAOLD = (fnMessageboxA)GetProcAddress(GetModuleHandle(L"User32.dll"), "MessageBoxA");
-	DetourAttach((void**)&MessageBoxAOLD, MessageBoxAEx);
-	if (DetourTransactionCommit() != NO_ERROR)
-	{
-		MessageBoxW(NULL, L"Æô¶¯Ê§°Ü", L"YukiYuki-System Ver0.94", MB_OK | MB_ICONWARNING);
-	}
-	else
-	{
-		MessageBoxW(NULL, L"±¾²¹¶¡ÓÉ¡¾°×¾®Ä¾Ñ§Ôº¡¿ÖÆ×÷\n½ûÖ¹Ò»ÇĞÂ¼²¥Ö±²¥ºÍÉÌÒµĞĞÎª", L"YukiYuki-System Ver0.94", MB_OK | MB_ICONINFORMATION);
-	}
+	DetourTransactionBegin();
+	DetourAttach(&g_pOldCreateFontIndirectA, NewCreateFontIndirectA);
+	//DetourAttach(&g_pOldTextOutA, NewTextOutA);
+	DetourAttach((void**)& MessageBoxAOLD, MessageBoxAEx);
+	if (DetourTransactionCommit() != NOERROR)
+		MessageBox(NULL, L"å¯åŠ¨å¤±è´¥", L"å¯åŠ¨å¤±è´¥", MB_OK);
 }
-
-BOOL APIENTRY DllMain(HMODULE hModule,
-	DWORD ul_reason_for_call,
-	LPVOID lpReserved)
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
 {
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		//must load texts before Hook!
-		BeginDetour();
-	case DLL_THREAD_ATTACH:
-		//Unload();
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+		Init();
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
 }
-
-extern "C" __declspec(dllexport) void dummy(void)
-{
+extern "C" __declspec(dllexport) void dummy(void) {
 	return;
 }
